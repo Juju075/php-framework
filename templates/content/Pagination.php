dea6d9faa9adcb6a67878fce0526e72cdb95f8ae
elgato <amzfba.1bestbuy@gmail.com>
2023-02-28 22:43:10 +0100

    cleanup

--- templates/content/Pagination.php
+++ templates/content/Pagination.php
@@ -43,32 +43,6 @@ class Pagination
         return $this;
     }
 
-    /**
-     * @throws NotFoundException
-     */
-    public function loadMore(?array $posts = [])
-    {
-        $currentPage = 1;
-        $offset = 0;
-        if (!empty($_GET['page']) && (int)$_GET['page'] !== 1) {
-            $currentPage = $_GET['page'];
-            $offset = $currentPage - 1;
-        }
-
-        $posts[] = (new \App\Framework\View\Pagination(PostRepository))->requestItems($offset);
-
-        $app = App::class;
-        $container = App::class->getContainer();
-        $route = App::class->getRoute();
-
-        //Relance index postcontroller
-        $instance = new ControllerResolver($container);
-        $page = $instance->resolve($route);
-        $page($posts);
-
-        // si re-clic
-        self::loadMore($posts);
-    }
 
     /**
      * @param int|null $offset
