39c4c2db39b39249877282907b558ad3cd6d17d9
elgato <amzfba.1bestbuy@gmail.com>
2023-02-28 20:30:38 +0100

    added README.md

--- config/const.php
+++ config/const.php
@@ -6,7 +6,7 @@ const password = 'root';
 define('TEMPLATE_DIRECTORY', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR);
 define('UPLOAD_IMAGE', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'upload');
 define('ENTITY_DIRECTORY', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Entity');
-//show', 'update', 'remove GET', 'PUT', 'DELETE
+
 
 
 
