1f39e9a0640e9dcbc0a9018d48e21bf5148b8e52
elgato <amzfba.1bestbuy@gmail.com>
2023-03-04 03:58:24 +0100

    Refactoring error code

--- src/Exception/ResourceNotFound.php
+++ src/Exception/ResourceNotFound.php
@@ -7,5 +7,6 @@ class ResourceNotFound extends  \Exception implements ExceptionInterface
     public function __construct($message = 'Resource not found')
     {
         parent::__construct($message = 'Resource not found',ErrorCode::HTTP_INTERNAL_SERVER_ERROR);
+
     }
 }
