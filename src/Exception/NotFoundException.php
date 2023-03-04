0bcef8f8ac797886df84237631d73eb93db50d79
elgato <amzfba.1bestbuy@gmail.com>
2023-03-04 03:59:15 +0100

    Refactoring error code

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -10,6 +10,5 @@ class NotFoundException extends \Exception implements ExceptionInterface
     public function __construct($message = "Page not found")
     {
         parent::__construct($message, ErrorCode::HTTP_NOT_FOUND);
-
     }
 }
