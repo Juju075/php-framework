4b013963efab6462e679cb63f60d03a097169a23
elgato <amzfba.1bestbuy@gmail.com>
2023-03-12 09:14:13 +0100

    Event Manager start coding

---
+++ src/Framework/Event/EventManager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Framework\Event;
+
+class EventManager implements EventDispatcherInterface
+{
+
+    public function dispatch(object $event)
+    {
+        // TODO: Implement dispatch() method.
+    }
+}
