39c30144cf247cbe7c960065a47845e90086f72c
elgato <amzfba.1bestbuy@gmail.com>
2023-03-18 13:34:05 +0100

    refactoring

--- config/const.php
+++ config/const.php
@@ -1,9 +1,4 @@
 <?php
-# dev! | will be replaced by Dotenv | already done DotEnv.php
-const DSN = 'mysql:host=localhost;dbname=blog_mvc;charset=utf8';
-const USERNAME = 'root';
-const PASSWORD = 'root';
-
 define('ENV_PATH', dirname(__DIR__) .
     DIRECTORY_SEPARATOR . 'src' .
     DIRECTORY_SEPARATOR . 'Framework' .
